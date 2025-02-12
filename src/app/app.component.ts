23bbe95553b63b049b5d45ace380b1d33f8c23fb
Arley <arley.rodriguez@isalus.es>
2025-02-12 13:02:14 +0100

    add initial components and styles for GifsApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from "./shared/components/aside/aside.component";
+import { HomePageComponent } from "./gifs/pages/home/home-page.component";
+import { CardListComponent } from "./gifs/components/card-list/card-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsideComponent, HomePageComponent, CardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
